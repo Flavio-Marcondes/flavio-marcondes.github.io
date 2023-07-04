045077cbfa95d5b5d5f8ce1ba460549b47d989ce
Flavio-Marcondes <138412546+Flavio-Marcondes@users.noreply.github.com>
2023-07-04 12:46:51 -0300

    Site v1.3

---
+++ js/interactionObserver.js
@@ -0,0 +1,31 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+
+        // Detects the right tag for the animation
+        if(entry.target.classList.contains('section1'))
+        {
+          // Add the animation class
+          entry.target.classList.add('space1');
+        }
+
+        // Detects the right tag for the animation
+        if(entry.target.classList.contains('section4'))
+        {
+          // Add the animation class
+          entry.target.classList.add('space4');
+        }
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('.section1'));
+  observer.observe(document.querySelector('.name'));
+  observer.observe(document.querySelector('.t1'));
+  observer.observe(document.querySelector('.t2'));
+  observer.observe(document.querySelector('.t3'));
+  observer.observe(document.querySelector('.t4'));
+  observer.observe(document.querySelector('.section4'));
+  
