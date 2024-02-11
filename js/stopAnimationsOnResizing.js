19dd13ef9d10c6c52eaaee4b8d195419001f81c0
Flavio-Marcondes <flaviomarcondesviana@gmail.com>
2024-02-10 21:03:17 -0300

    Site v1.5

---
+++ js/stopAnimationsOnResizing.js
@@ -0,0 +1,6 @@
+window.addEventListener("resize", () => {
+  document.getElementsByClassName('t1')[0].classList.add('stopAnimation');
+  document.getElementsByClassName('t2')[0].classList.add('stopAnimation');
+  document.getElementsByClassName('t3')[0].classList.add('stopAnimation');
+  document.getElementsByClassName('t4')[0].classList.add('stopAnimation');
+});
