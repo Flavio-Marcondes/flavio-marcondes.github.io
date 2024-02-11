77900f7ea771b1b34c56f9b8db7c420d5741497b
Flavio <flaviomarcondesviana@gmail.com>
2024-02-10 21:41:28 -0300

    Fixed merging bugs + Site v1.5

--- js/interactionObserver.js
+++ js/interactionObserver.js
@@ -24,29 +24,3 @@ const observer = new IntersectionObserver(entries => {
   observer.observe(document.querySelector('.section1'));
   observer.observe(document.querySelector('.section4'));
   
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-
-        // Detects the right tag for the animation
-        if(entry.target.classList.contains('section1'))
-        {
-          // Add the animation class
-          entry.target.classList.add('space1');
-        }
-
-        // Detects the right tag for the animation
-        if(entry.target.classList.contains('section4'))
-        {
-          // Add the animation class
-          entry.target.classList.add('space4');
-        }
-      }
-    });
-  });
-  
-  observer.observe(document.querySelector('.section1'));
-  observer.observe(document.querySelector('.section4'));
-  
--- js/stopAnimationsOnResizing.js
+++ js/stopAnimationsOnResizing.js
@@ -4,9 +4,3 @@ window.addEventListener("resize", () => {
   document.getElementsByClassName('t3')[0].classList.add('stopAnimation');
   document.getElementsByClassName('t4')[0].classList.add('stopAnimation');
 });
-window.addEventListener("resize", () => {
-  document.getElementsByClassName('t1')[0].classList.add('stopAnimation');
-  document.getElementsByClassName('t2')[0].classList.add('stopAnimation');
-  document.getElementsByClassName('t3')[0].classList.add('stopAnimation');
-  document.getElementsByClassName('t4')[0].classList.add('stopAnimation');
-});
